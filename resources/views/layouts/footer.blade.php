2c164a01076b34a99effeb737ad07c8126ebc618
Miguel Antonio <miguelbragaantonio28@gmail.com>
2025-03-04 23:25:32 -0300

    Estilizando o front end e adicionando novas funcionalidades

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,40 @@
+<footer class="bg-white rounded-lg shadow-sm dark:bg-gray-900 w-screen">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <a href="/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <img src="/build/assets/img/logocine.png" class="h-8" alt=" "/>
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Cineflix</span>
+            </a>
+
+
+            <ul class="flex flex-wrap items-center text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                <li><a href="#" class="hover:underline me-4 md:me-6">Sobre nós</a></li>
+                <li><a href="#" class="hover:underline me-4 md:me-6">Política de Privacidade</a></li>
+                <li><a href="#" class="hover:underline me-4 md:me-6">Licença</a></li>
+                <li><a href="#" class="hover:underline me-4 md:me-6">Contato</a></li>
+
+
+                <li>
+                    <a href="https://github.com/nahbchi" target="_blank" class="flex items-center ms-4 text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                        <i data-lucide="github" class="w-5 h-5"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/miguel-ant%C3%B4nio-braga-6a4784291/" target="_blank" class="flex items-center ms-4 text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                        <i data-lucide="linkedin" class="w-5 h-5"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"/>
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © 2025 <a href="/" class="hover:underline">Cineflix™</a> Todos direitos Reservados
+        </span>
+    </div>
+</footer>
+
+<script src="https://unpkg.com/lucide@latest"></script>
+<script>
+    lucide.createIcons();
+</script>
--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -1,2 +1,3 @@
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+
